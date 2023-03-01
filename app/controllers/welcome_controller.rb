dd2dcbfed3a42837d4a5689e55d32fb89c9fc1c4
Henrique Souza dos Santos <h.s.s_henrique@hotmail.com>
2023-03-01 20:49:18 -0300

    instance variables and query params

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,12 @@
 
 # Welcome controller
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    @name = "Henrique"
+
+    @god_knowledge = "Batman"
+
+    # after using http://127.0.0.1:3000/?god_velocity=Flash
+    @god_velocity = params[:god_velocity] #=> Flash
+  end
 end
